baccf110458fc72d311e3b10cb74191bcb880517
KBPheko <@J8riW2aG*>
2023-05-26 15:18:22 +0200

    first commit

---
+++ Employee Management System/EmployeeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmployeeTableViewCell.swift
+//  Employee Management System
+//
+//  Created by DA MAC M1 151 on 2023/05/24.
+//
+
+import UIKit
+
+class EmployeeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Employee Management System/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Employee Management System
+//
+//  Created by DA MAC M1 151 on 2023/05/24.
+//
+
+import Foundation
